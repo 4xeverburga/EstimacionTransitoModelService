67202c5005aa8862f51f6ad72dccaad133de19c1
4xeverburga <ever.burga.p@uni.pe>
2023-12-03 12:30:40 -0500

    added: pkl model

--- model/model.py
+++ model/model.py
@@ -1,2 +1,11 @@
+import pickle
 def main():
-	pass
+	# import lda model from sotored_models/
+	lda_model = pickle.load(open('model/stored_models/lda.pkl', 'rb'))
+	# test a value
+	print(lda_model.predict([[-69]]))
+if __name__ == '__main__':
+	#print base path 
+	import os
+	print(os.getcwd())
+	main()
