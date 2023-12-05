4cabb1b9901e7e434ace24044aa5d846a23479fc
4xeverburga <ever.burga.p@uni.pe>
2023-12-04 23:08:03 -0500

    a

--- model/model.py
+++ model/model.py
@@ -4,6 +4,9 @@ def main():
 	lda_model = pickle.load(open('model/stored_models/lda.pkl', 'rb'))
 	# test a value
 	print(lda_model.predict([[-69]]))
+	val_set = [(-33,0),(-33,0),(-33,0),(-34,0),(-69,1),(-68,1),(-70,1),(67,1),(-72,2),(-73,2),(-73,2),(-72,2)]
+	# calculate accuracy
+	
 if __name__ == '__main__':
 	#print base path 
 	import os
